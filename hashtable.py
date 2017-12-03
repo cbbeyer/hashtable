a8490e058f20f7f2628cc05d67586ad99e0c3ac7
Carsen Beyer <carsen.beyer@gmail.com>
2017-12-02 19:02:28 -0700

    new changes

--- hashtable.py
+++ hashtable.py
@@ -18,7 +18,6 @@ class Hashtable(ABC):
 
     def get(self, key):
         index = self.get_hash(key)
-
         return self.buckets[index].get(key)
 
 
@@ -41,10 +40,15 @@ class StringHashtable(Hashtable):
                 temp_val = len(key)
 
             temp_val = temp_val * 17
+            temp_val = temp_val - len(key)
             ascii_values += str(temp_val)
 
         ascii_values = int(ascii_values) / 3
-        index = int(ascii_values) % 9
+        ascii_values * 3 * len(key)
+        index = int(ascii_values) % 11 - 1
+
+        if index < 0:
+            index = 0
 
         return index
 
@@ -58,26 +62,97 @@ class StringHashtable(Hashtable):
 
 
 class GuidHashtable(Hashtable):
-    '''String hashtable class'''
+    '''Guid hashtable class'''
 
     def get_hash(self, key):
-        pass
+        temp_hex = key[16:]
+        temp_hex = temp_hex[:8]
+
+        temp_hex = bin(int(temp_hex, 16))[2:]
+
+        print(key)
+
+        print(temp_hex)
+
+        counter = 0
+        count_hash = ''
+
+        print('------------------')
+        for c in temp_hex:
+
+            if c == '1':
+                print(c, '--')
+                counter += 1
+                counter = counter * 3
+            else:
+                counter = 3
+
+            c = c * 3 * counter
+
+            count_hash += str(c)
+
+
+
+
+            count_hash += str(counter)
+
+        print('------------------')
+        print(count_hash)
+
+        index = int(count_hash) % 11 - 1
+
+        if index < 0:
+            index = 0
+
+        return index
+
+
+        # for c in temp_hash:
+        #     if c == 1 or c == 3 or c == 5:
+        #         temp_val = int(c) * 13
+        #         new_hash += str(temp_val)
+        #     elif c == 0 or c == 2 or c == 4:
+        #         temp_val = int(c) * 17
+        #         new_hash += str(temp_val)
+        #     else:
+        #         temp_val = int(c) * 3
+        #         new_hash += str(temp_val)
+
+        # index = int(new_hash) % 11 - 1
+        #
+        # print(index)
 
 
         # returns a number 0-9
 
     def debug_print(self):
-        pass
+        for i in range(len(self.buckets)):
+            if self.buckets[i].root is not None:
+                li = self.buckets[i].walk_bfs_return(self.buckets[i].root)
+                print('{}: {}'.format(i, ', '.join(li), end=''))
+            else:
+                print('{}: {}'.format(i, self.buckets[i].root))
 
 
 class ImageHashtable(Hashtable):
-    '''String hashtable class'''
+    '''Image hashtable class'''
 
     def get_hash(self, key):
-        pass
+        with open("images/{}".format(key), "rb") as binary_file:
+            f = binary_file.read()
 
+            i = int.from_bytes(f, byteorder='big')
+            index = i % 11 - 1
 
-        # returns a number 0-9
+            if index < 0:
+                index = 0
+
+            return index
 
     def debug_print(self):
-        pass
+        for i in range(len(self.buckets)):
+            if self.buckets[i].root is not None:
+                li = self.buckets[i].walk_bfs_return(self.buckets[i].root)
+                print('{}: {}'.format(i, ', '.join(li), end=''))
+            else:
+                print('{}: {}'.format(i, self.buckets[i].root))
--- main.py
+++ main.py
@@ -19,20 +19,55 @@ def main():
     #
     # string_hash_table.debug_print()
 
-    with open('strings.txt', newline='') as f:
+    # with open('strings.txt', newline='') as f:
+    #     for line_i, line in enumerate(f):
+    #         line = line.rstrip()
+    #         value = line.lower()
+    #         string_hash_table.set(line, value)
+    #
+    # print('String hash table:')
+    # string_hash_table.debug_print()
+    #
+    # print()
+    # print('String Lookups:')
+    # print(string_hash_table.get('Indian Meal Moth'))
+    # print(string_hash_table.get('Orb-Weaving Spiders (Banded Garden Spider)'))
+    #
+    # print()
+
+    guid_hash_table = GuidHashtable()
+
+    with open('guids.txt', newline='') as f:
         for line_i, line in enumerate(f):
             line = line.rstrip()
             value = line.lower()
-            string_hash_table.set(line, value)
+            # guid_hash_table.set(line, value)
+            guid_hash_table.set(line, line)
 
-    string_hash_table.debug_print()
+    guid_hash_table.debug_print()
 
-    # Should we be looking up on the key? or the value?
-    print()
-    print(string_hash_table.get('Indian Meal Moth'))
-    print(string_hash_table.get('Orb-Weaving Spiders (Banded Garden Spider)'))
+    # guid_hash_table.get_hash('00000158691797bd77464872000a0018001b000c')
+    # guid_hash_table.get_hash('00000158691797bd77464873000a0018001b000c')
+    # guid_hash_table.get_hash('00000158691797bd77464874000a0018001b000c')
+    # guid_hash_table.get_hash('00000158691797bd7746487c000a001800388ccf')
 
-    print()
+    # images_hash_table = ImageHashtable()
+    #
+    # with open('images.txt', newline='') as f:
+    #     for line_i, line in enumerate(f):
+    #         line = line.rstrip()
+    #         value = line.lower()
+    #         images_hash_table.set(line, line)
+    #
+    # print('Image hash table:')
+    # images_hash_table.debug_print()
+    #
+    # print()
+    # print('Image lookups:')
+    # print(images_hash_table.get('document.png'))
+    # print(images_hash_table.get('security_keyandlock.png'))
+
+
 
 # Runner
 if __name__ == '__main__':
